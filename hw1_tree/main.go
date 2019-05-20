f426849780dc6858c5046dc570a41552cb7941ae
Anuar Serdaliyev <Anuar_Serdaliyev@epam.com>
2019-05-20 18:56:44 +0200

    week1 almost done

---
+++ hw1_tree/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	//"path/filepath"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(w io.Writer, p string, files bool) error {
+	identCount := strings.Count(p, "//")
+	var tabs, prefix string
+	dirList, err := ioutil.ReadDir(p)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < identCount; i++ {
+		tabs += "|   "
+	}
+
+	for i, dir := range dirList {
+		if i != len(dirList)-1 {
+			prefix = "├───"
+		} else {
+			prefix = "└───"
+		}
+		dirStr := tabs + prefix + dir.Name()
+
+		fmt.Fprintln(w, dirStr)
+		// else: print normal
+		dirPath := p + "//" + dir.Name()
+		dirTree(w, dirPath, files)
+	}
+	return nil
+}
