ecf0aed91fc176a829326e950c7cad0ab5c7b26b
Anuar Serdaliyev <Anuar_Serdaliyev@epam.com>
2019-05-30 17:57:02 +0200

    multithreading

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -2,43 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
+type PipeData struct {
+	sync.Mutex
+	Value      string
+	CrcHash    string
+	CrcMd5Hash string
+	TildaHash  string
+	MultiHash  []string
+}
+
 // ExecutePipeline makes pipiline of funcs
-func ExecutePipeline(jobfuncs ...job) {
+func ExecutePipeline(jobFuncs ...job) {
 	in := make(chan interface{})
-	for _, jobFunc := range jobfuncs {
+	out := make(chan interface{})
+	for i := 0; i < len(jobFuncs)-1; i++ {
 		out := make(chan interface{})
-		go jobFunc(in, out)
+		go jobFuncs[i](in, out)
 		in = out
 	}
-	time.Sleep(100 * time.Second)
+	jobFuncs[len(jobFuncs)-1](in, out)
 }
 
 // SingleHash accepts in channel in makes operations and send to out
 func SingleHash(in, out chan interface{}) {
 	var result string
+	pch := make(chan PipeData)
 LOOP:
 	for {
 		select {
-		case data <- in:
+		case data := <-in:
 			dataStr := strconv.Itoa(data.(int))
-			fmt.Println(dataStr, "SingleHash", "data", dataStr)
-			md5Hash := DataSignerMd5(dataStr)
-			fmt.Println(dataStr, "SingleHash", "md5(data)", md5Hash)
-			crcMd5Hash := DataSignerCrc32(md5Hash)
-			fmt.Println(dataStr, "SingleHash", "crc32(md5(data))", crcMd5Hash)
-			crcHash := DataSignerCrc32(dataStr)
-			fmt.Println(dataStr, "SingleHash", "crc32(data)", crcHash)
-			result = crcHash + "~" + crcMd5Hash
-			fmt.Println(dataStr, "SingleHash", "result", result)
-			out <- result
-		case <- time.After(5*time.Millisecond):
-			break LOOP
+			pd := PipeData{Value: dataStr}
+			go CalcSingleHash(pd, pch)
+		case <-time.After(5 * time.Millisecond):
 			out <- "end"
+			break LOOP
 		}
 	}
 }
@@ -49,14 +54,19 @@ func MultiHash(in, out chan interface{}) {
 	for data := range in {
 		result = ""
 		dataStr := data.(string)
-		for th := 0; th < 6; th++ {
-			thStr := strconv.Itoa(th)
-			crcStr := DataSignerCrc32(thStr + dataStr)
-			result += crcStr
-			fmt.Println(dataStr, "MultiHash: crc32(th+step1))", th, crcStr)
+		if dataStr != "end" {
+			for th := 0; th < 6; th++ {
+				thStr := strconv.Itoa(th)
+				crcStr := DataSignerCrc32(thStr + dataStr) // Paralell
+				result += crcStr
+				fmt.Println(dataStr, "MultiHash: crc32(th+step1))", th, crcStr)
+			}
+			fmt.Println(dataStr, "MultiHash result:", result)
+			out <- result
+		} else {
+			out <- dataStr
+			break
 		}
-		fmt.Println(dataStr, "MultiHash result:", result)
-		out <- result
 	}
 }
 
@@ -64,16 +74,34 @@ func MultiHash(in, out chan interface{}) {
 func CombineResults(in, out chan interface{}) {
 	var result string
 	var sl []string
-	for {
-		select {
-		case data := <-in:
-			dataStr := data.(string)
+	for data := range in {
+		dataStr := data.(string)
+		if dataStr != "end" {
 			sl = append(sl, dataStr)
-		case 
+		} else {
+			sort.SliceStable(sl, func(i, j int) bool { return sl[i] < sl[j] })
+			result = strings.Join(sl, "_")
+			fmt.Println("CombineResults", result)
+			break
 		}
 	}
-
-	result = strings.Join(sl, "_")
-	fmt.Println("CombineResults", result)
 	out <- result
+}
+
+// CalcSingleHash calculates SingleHash value for one data entry
+func CalcSingleHash(pd PipeData, pch chan PipeData) {
+	fmt.Println(pd.Value, "SingleHash", "data", pd.Value)
+	md5Hash := DataSignerMd5(pd.Value) // Lock
+	fmt.Println(pd.Value, "SingleHash", "md5(data)", md5Hash)
+	pd.CrcMd5Hash = DataSignerCrc32(md5Hash) // Paralell
+	fmt.Println(pd.Value, "SingleHash", "crc32(md5(data))", pd.CrcMd5Hash)
+
+	// sync this with parent
+	go func() {
+		pd.CrcHash = DataSignerCrc32(pd.Value) // Paralell
+		fmt.Println(pd.Value, "SingleHash", "crc32(data)", pd.CrcHash)
+	}()
+
+	pd.TildaHash = pd.CrcHash + "~" + pd.CrcMd5Hash
+	fmt.Println(pd.Value, "SingleHash", "result", pd.TildaHash)
 }
