4ea5bff62c78051647af7ae9341f733e23591f49
Anuar Serdaliyev <Anuar_Serdaliyev@epam.com>
2019-06-03 13:08:10 +0200

    3.08s

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -33,7 +33,8 @@ func ExecutePipeline(jobFuncs ...job) {
 
 // SingleHash accepts in channel in makes operations and send to out
 func SingleHash(in, out chan interface{}) {
-	wg := new(sync.WaitGroup)
+	wg := &sync.WaitGroup{}
+	m := new(sync.Mutex)
 LOOP:
 	for {
 		select {
@@ -41,29 +42,30 @@ LOOP:
 			wg.Add(1)
 			dataStr := strconv.Itoa(data.(int))
 			pd := PipeData{Value: dataStr}
-			go CalcSingleHash(&pd, out, wg)
+			go CalcSingleHash(&pd, out, wg, m)
 		case <-time.After(5 * time.Millisecond):
 			break LOOP
 		}
 	}
 	wg.Wait()
-	out <- struct{}{}
+	close(out)
 }
 
 // MultiHash creates 6 hashes from one input
 func MultiHash(in, out chan interface{}) {
-	wg := new(sync.WaitGroup)
+	wg := &sync.WaitGroup{}
 	for data := range in {
+		//fmt.Println("DEBUG: Entered chan cycle")
 		wg.Add(1)
 		pd, ok := data.(PipeData)
-		if ok {
-			go CalcMultiHash(&pd, out, wg)
-		} else {
-			break
+		if !ok {
+			fmt.Printf("MHASH: Type assertion error. Got: %v\n", data)
 		}
+		go CalcMultiHash(&pd, out, wg)
 	}
+
 	wg.Wait()
-	out <- struct{}{}
+	close(out)
 }
 
 // CombineResults concatenates result with "_" separator
@@ -72,29 +74,35 @@ func CombineResults(in, out chan interface{}) {
 	var sl []string
 	for data := range in {
 		pd, ok := data.(PipeData)
-		if ok {
-			sl = append(sl, pd.MHashes)
-		} else {
-			sort.SliceStable(sl, func(i, j int) bool { return sl[i] < sl[j] })
-			result = strings.Join(sl, "_")
-			fmt.Println("CombineResults", result)
-			break
+		if !ok {
+			fmt.Printf("COMBINE: Type assertion error. Got: %v\n", data)
 		}
+		sl = append(sl, pd.MHashes)
 	}
+	sort.SliceStable(sl, func(i, j int) bool { return sl[i] < sl[j] })
+	result = strings.Join(sl, "_")
+	fmt.Println("CombineResults", result)
 	out <- result
+	close(out)
 }
 
 // CalcSingleHash calculates SingleHash value for one data entry
-func CalcSingleHash(pd *PipeData, out chan interface{}, wg *sync.WaitGroup) {
+func CalcSingleHash(pd *PipeData, out chan interface{}, wg *sync.WaitGroup, m *sync.Mutex) {
 	waitCh := make(chan struct{})
 	fmt.Println(pd.Value, "SingleHash", "data", pd.Value)
-	md5Hash := DataSignerMd5(pd.Value) // Lock
+	m.Lock()
+	md5Hash := DataSignerMd5(pd.Value)
+	m.Unlock()
 	fmt.Println(pd.Value, "SingleHash", "md5(data)", md5Hash)
+	pd.Lock()
 	pd.CrcMd5Hash = DataSignerCrc32(md5Hash)
+	pd.Unlock()
 	fmt.Println(pd.Value, "SingleHash", "crc32(md5(data))", pd.CrcMd5Hash)
 
 	go func() {
+		pd.Lock()
 		pd.CrcHash = DataSignerCrc32(pd.Value)
+		pd.Unlock()
 		fmt.Println(pd.Value, "SingleHash", "crc32(data)", pd.CrcHash)
 		waitCh <- struct{}{}
 	}()
@@ -103,23 +111,26 @@ func CalcSingleHash(pd *PipeData, out chan interface{}, wg *sync.WaitGroup) {
 
 	pd.TildaHash = pd.CrcHash + "~" + pd.CrcMd5Hash
 	fmt.Println(pd.Value, "SingleHash", "result", pd.TildaHash)
-	out <- pd
+	out <- *pd
 	wg.Done()
 }
 
 // CalcMultiHash calculates hashes in interation
 func CalcMultiHash(pd *PipeData, out chan interface{}, wg *sync.WaitGroup) {
-	wg2 := new(sync.WaitGroup)
+	wg2 := &sync.WaitGroup{}
 	thNum := 6
+	pd.MHash = make(map[int]string)
 	for th := 0; th < thNum; th++ {
 		wg2.Add(1)
-		thStr := strconv.Itoa(th)
-		go func() {
+		go func(th int) {
+			thStr := strconv.Itoa(th)
 			crcStr := DataSignerCrc32(thStr + pd.TildaHash)
+			pd.Lock()
 			pd.MHash[th] = crcStr
-			fmt.Println(pd.TildaHash, "MultiHash: crc32(th+step1))", th, crcStr)
+			pd.Unlock()
+			fmt.Println(pd.TildaHash, "MultiHash: crc32(th+step1))", thStr, crcStr)
 			wg2.Done()
-		}()
+		}(th)
 	}
 
 	wg2.Wait()
@@ -129,6 +140,6 @@ func CalcMultiHash(pd *PipeData, out chan interface{}, wg *sync.WaitGroup) {
 	}
 
 	fmt.Println(pd.TildaHash, "MultiHash result:", pd.MHashes)
-	out <- pd
+	out <- *pd
 	wg.Done()
 }
